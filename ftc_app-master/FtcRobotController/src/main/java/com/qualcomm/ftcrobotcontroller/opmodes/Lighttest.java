0c680ed6b31872e27bf1eefdae7dc99ab080eb51
MakutaTeridax <heel7284@gmail.com>
2015-10-29 20:16:05 -0700

    Added the part to make robot stop when red line is reached

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Lighttest.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Lighttest.java
@@ -7,8 +7,35 @@ import com.qualcomm.hardware.HiTechnicNxtLightSensor;
  * Created by heel7_000 on 10/29/2015.
  */
 public class Lighttest extends LinearOpMode {
+
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+    HiTechnicNxtLightSensor light;
+
     @Override
+
     public void runOpMode() throws InterruptedException {
-        
+        leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+
+        int i = 1;
+
+        while(i==1) {
+            leftMotor.setPower(1);
+            rightMotor.setPower(1);
+            if (light.getLightDetected()>50){
+                leftMotor.setPower(0);
+                rightMotor.setPower(0);
+                i++;
+            }
+            else{
+                leftMotor.setPower(1);
+                rightMotor.setPower(1);
+
+            }
+        }
     }
+
+
+
 }
