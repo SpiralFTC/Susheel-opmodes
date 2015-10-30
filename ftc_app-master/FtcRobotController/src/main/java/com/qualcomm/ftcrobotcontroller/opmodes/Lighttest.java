77420c1a4a96ca2cc99214521c27ad7d6d7c0345
MakutaTeridax <heel7284@gmail.com>
2015-10-29 19:48:18 -0700

    Begainings of Light testing

---
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Lighttest.java
@@ -0,0 +1,14 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.hardware.HiTechnicNxtLightSensor;
+
+/**
+ * Created by heel7_000 on 10/29/2015.
+ */
+public class Lighttest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        
+    }
+}
