5134e6d7416878f89d0811c896960cea563cf2e2
MakutaTeridax <heel7284@gmail.com>
2015-11-01 15:23:54 -0800

    Added 4th Mortor and created EOPD test Program

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Lighttest.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Lighttest.java
@@ -10,6 +10,8 @@ public class Lighttest extends LinearOpMode {
 
     DcMotor leftMotor;
     DcMotor rightMotor;
+    DcMotor leftMotor1;
+    DcMotor rightMotor1;
     HiTechnicNxtLightSensor light;
 
     @Override
@@ -17,20 +19,30 @@ public class Lighttest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         leftMotor = hardwareMap.dcMotor.get("leftMotor");
         rightMotor = hardwareMap.dcMotor.get("rightMotor");
+        leftMotor1 = hardwareMap.dcMotor.get("leftMotor1");
+        rightMotor1 = hardwareMap.dcMotor.get("rightMotor1");
+
+
 
         int i = 1;
 
         while(i==1) {
             leftMotor.setPower(1);
             rightMotor.setPower(1);
+            leftMotor1.setPower(1);
+            rightMotor1.setPower(1);
             if (light.getLightDetected()>50){
                 leftMotor.setPower(0);
                 rightMotor.setPower(0);
+                leftMotor1.setPower(0);
+                rightMotor1.setPower(0);
                 i++;
             }
             else{
-                leftMotor.setPower(1);
-                rightMotor.setPower(1);
+                leftMotor.setPower(0);
+                rightMotor.setPower(0);
+                leftMotor1.setPower(0);
+                rightMotor1.setPower(0);
 
             }
         }
